1725e548e9b0215b5dd8521a787aebb9e2d31efa
aldoyfa <96334323+aldoyfa@users.noreply.github.com>
2025-05-29 00:36:56 +0700

    feat: added placeholder map and collissions

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,17 +2,44 @@ package main;
 
 import javax.swing.JPanel;
 import java.awt.Dimension;
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import inputs.KeyboardListener;
+import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
-    Thread gameThread;
+
+    // Screen Settings
+    final int originalTileSize = 16; // 16x16 tiles
+    final int scale = 4; // Scale factor
+
+    public final int tileSize = originalTileSize * scale; // 64x64 tiles
+    public final int maxScreenCol = 20;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol; // 1280 pixels
+    public final int screenHeight = tileSize * maxScreenRow; // 768 pixels
     
+    // World Settings 
+    public final int maxWorldCol = 52;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol; 
+    public final int worldHeight = tileSize * maxWorldRow; 
+
+    // FPS
+    int FPS = 60; 
+
+    // Game Thread
+    Thread gameThread;
+    public TileManager tileM = new TileManager(this);
+    KeyboardListener keyH = new KeyboardListener();
+    public Player player = new Player(this, keyH);
+
     public GamePanel() {
-        this.setPreferredSize(new Dimension(1535, 1536)); // pixel per tile = 16; scale = 3; pixel per tile = 48; jumlah tile = 32; total pixel = 1536
-        this.setBackground(Color.white);
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -22,21 +49,37 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
+        double drawInterval = 1000000000 / FPS; 
+        double nextDrawTime = System.nanoTime() + drawInterval;
         while (gameThread != null) {
             update(); 
             repaint(); 
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime /= 1000000; 
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+                nextDrawTime += drawInterval; 
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void update() {
-        // Update game logic here
+        player.update(); 
     }
 
-    public void drawComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(Color.WHITE);
-        g2.fillRect(100, 100, 48, 48); 
+        tileM.draw(g2);
+        player.draw(g2);
         g2.dispose(); 
     }
 }
